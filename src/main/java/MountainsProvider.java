c24f982b75b14d6652daf67ba8d3ef57a42aeea7
meetcshah19 <meetcshah19@gmail.com>
2020-03-22 03:23:11 +0530

    Complex Terrain

---
+++ src/main/java/MountainsProvider.java
@@ -0,0 +1,55 @@
+import org.terasology.math.TeraMath;
+import org.terasology.math.geom.BaseVector2i;
+import org.terasology.math.geom.Rect2i;
+import org.terasology.math.geom.Vector2f;
+import org.terasology.utilities.procedural.BrownianNoise;
+import org.terasology.utilities.procedural.Noise;
+import org.terasology.utilities.procedural.PerlinNoise;
+import org.terasology.utilities.procedural.SubSampledNoise;
+import org.terasology.world.generation.Facet;
+import org.terasology.world.generation.FacetProvider;
+import org.terasology.world.generation.GeneratingRegion;
+import org.terasology.world.generation.Updates;
+import org.terasology.world.generation.facets.SurfaceHeightFacet;
+
+/*
+ * Copyright 2020 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@Updates(@Facet(SurfaceHeightFacet.class))
+public class MountainsProvider implements FacetProvider {
+
+    private Noise mountainNoise;
+
+    @Override
+    public void setSeed(long seed) {
+        mountainNoise = new SubSampledNoise(new BrownianNoise(new PerlinNoise(seed + 2), 2), new Vector2f(0.001f, 0.001f), 1);
+    }
+
+    @Override
+    public void process(GeneratingRegion region) {
+        SurfaceHeightFacet facet = region.getRegionFacet(SurfaceHeightFacet.class);
+        float mountainHeight = 40;
+        // loop through every position on our 2d array
+        Rect2i processRegion = facet.getWorldRegion();
+        for (BaseVector2i position : processRegion.contents()) {
+            // scale our max mountain height to noise (between -1 and 1)
+            float additiveMountainHeight = mountainNoise.noise(position.x(), position.y()) * mountainHeight;
+            // don't bother subtracting mountain height, that will allow unaffected regions
+            additiveMountainHeight = TeraMath.clamp(additiveMountainHeight, 0, mountainHeight);
+
+            facet.setWorld(position, facet.getWorld(position) + additiveMountainHeight);
+        }
+    }
+}
